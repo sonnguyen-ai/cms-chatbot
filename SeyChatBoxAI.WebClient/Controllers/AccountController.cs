5370f9778e7456185d2b8e3a4b7b25bb14d9e1f8
Duc Ly (Jackie Ly) <k.duchappy@gmail.com>
2025-02-27 16:45:59 +0700

    Completed UI of pages:   - Login   - Dashboard

---
+++ SeyChatBoxAI.WebClient/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SeyChatBoxAI.WebClient.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("login")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SeyChatBoxAI.WebClient/Controllers/BuildAIChatBotController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SeyChatBoxAI.WebClient.Controllers
+{
+    [Route("/build-ai-chatbot")]
+    public class BuildAIChatBotController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
