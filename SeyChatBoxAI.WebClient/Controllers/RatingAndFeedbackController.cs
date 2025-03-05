194acbb70739f50611121f489c43be282facf6b2
Duc Ly (Jackie Ly) <k.duchappy@gmail.com>
2025-03-05 17:05:33 +0700

    Completed UIs:   - Chats   - Rating & Feedback

---
+++ SeyChatBoxAI.WebClient/Controllers/RatingAndFeedbackController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SeyChatBoxAI.WebClient.Controllers
+{
+    [Route("/rating-feedback")]
+    public class RatingAndFeedbackController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
